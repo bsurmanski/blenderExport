19216ea49a169f210f6d9050ab49a391e02dc3d1
Brandon Surmanski <b.surmanski@gmail.com>
2015-01-22 00:28:23 -0500

    updated 'mdl' export for blender 2.7.2

--- io_export_mdl.py
+++ io_export_mdl.py
@@ -1,9 +1,18 @@
+bl_info = {
+    "name":         "MDL File Format",
+    "author":       "Brandon Surmanski",
+    "blender":      (2,7,2),
+    "version":      (0,0,1),
+    "location":     "File > Import-Export",
+    "description":  "Export custom MDL format",
+    "category":     "Import-Export"
+}
+
 import bpy
 from bpy_extras.io_utils import ExportHelper
 from mathutils import *
 from math import *
 import struct
-from blender_sharelib import *
 
 """
 MDL file format export
@@ -39,6 +48,68 @@ MDL:
     VERTS,
     FACES
 """
+
+#
+# SHARELIB
+#
+def normalizeAll():
+    for m in bpy.data.meshes:
+        for v in m.vertices:
+            sum = 0.0
+            for i in range(0, 3):
+                sum += v.co[i] * v.co[i]
+            sum = sqrt(sum)
+            for i in range(0, 3):
+                v.co[i] = v.co[i] / sum
+
+#sperically project onto plane z=1, produces x/y coords: {-1 <= x <= 1}
+def planet_co_to_uv():
+    sqrt2 = sqrt(2)
+    sqrt3inv = 1/sqrt(3)
+    for m in bpy.data.meshes:
+        for v in m.vertices:
+            ratio = 1 / v.co[2]
+            for i in range(0, 3):
+                v.co[i] = v.co[i] * (ratio)
+
+def float_to_ushort(val):
+    if val >= 1.0:
+        return 2**16-1
+    if val <= 0.0:
+        return 0
+    return int(floor(val * (2**16-1)))
+
+def float_to_short(val):
+    if val >= 1.0:
+        return 2**15-1
+    if val <= -1.0:
+        return -(2**15)+1
+    return int(round(val * (2**15-1)))
+
+def float_to_ubyte(val):
+    if val >= 1.0:
+        return 2**8-1
+    if val <= 0.0:
+        return 0
+    return int(round(val * (2**8-1)))
+
+def vec2_to_uhvec2(val):
+    return tuple((float_to_ushort(val[0]), float_to_ushort(val[1])))
+
+def vec3_to_hvec3(val):
+    return tuple((float_to_short(val[0]), float_to_short(val[1]), float_to_short(val[2])))
+
+def is_trimesh(mesh):
+    ret = True
+    for face in mesh.tessfaces:
+        if len(face.vertices) > 3:
+            ret = False
+            break
+    return ret
+
+#
+#
+#
 
 def uv_entry_tuple(mesh, facei, uvi):
     face = mesh.tessfaces[facei]
@@ -169,15 +240,7 @@ def write_mdl_mesh(obj, settings):
     write_mdl_verts(buf, obj, vlist, blist)
     write_mdl_faces(buf, mesh, flist)
     return b''.join(buf)
-    
 
-def is_trimesh(mesh):
-    ret = True
-    for face in mesh.tessfaces:
-        if len(face.vertices) > 3:
-            ret = False
-            break
-    return ret
 
 # ExportHelper is a helper class, defines filename and
 # invoke() function which calls the file selector.
@@ -222,12 +285,10 @@ class MdlExport(Operator, ExportHelper):
 
         obj = context.object
 
-        
         f = open(self.filepath, 'wb')
         obuf = write_mdl_mesh(obj, None)
         f.write(obuf)
         f.close()
-        
         return {'FINISHED'}
 
 
@@ -237,7 +298,8 @@ def menu_func_export(self, context):
 
 
 def register():
-    bpy.utils.register_class(MdlExport)
+    #bpy.utils.register_class(MdlExport)
+    bpy.utils.register_module(__name__)
     bpy.types.INFO_MT_file_export.append(menu_func_export)
 
 
@@ -250,4 +312,4 @@ if __name__ == "__main__":
     register()
 
     # test call
-    bpy.ops.export.mdl('INVOKE_DEFAULT')
+    #bpy.ops.export.mdl('INVOKE_DEFAULT')
