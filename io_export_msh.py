6ff3218d75e94f634e6758602d1e454f80856dd2
Brandon Surmanski <b.surmanski@gmail.com>
2016-05-14 16:22:17 -0700

    Change Mesh format to proper winged edge
    Loader will need to convert to graphics format

--- io_export_msh.py
+++ io_export_msh.py
@@ -21,20 +21,22 @@ MSH file format export
 
 HEADER:
     3 byte: magic number (MDL)
-    1 byte: version number (5)
-    4 byte: number of verts
-    4 byte: number of faces
-    4 byte: number of edges
-    1 byte: number of bones
+    1 byte: version number (6)
+    2 byte: number of verts
+    2 byte: number of uvs
+    2 byte: number of faces
+    2 byte: number of edges
+    2 byte: number of bones
+    2 byte: padding
     15 byte: name
+    1 byte: NULL
     32
 
 VERT:
     12 byte: position (3 * 4 byte float)
-    6 byte: normal (3 * 2 byte signed short) (normalized from -32768 to 32767) #TODO implicit Z
-    4 byte: uv coordinate (2 * 2 byte) (normalized from 0 to 65535)
-    3 byte: RGB vertex color
-    1 byte: material index
+    6 byte: normal (3 * signed short) (normalized from -32768 to 32767) #TODO implicit Z
+    4 byte: padding (OR UV position)
+    4 byte: padding (OR RGB vertex color and material index)
     1 byte: boneID 1
     1 byte: boneID 2
     1 byte: bone weight 1
@@ -42,25 +44,39 @@ VERT:
     2 byte: incident edge id
     32
 
-FACE:
-    6 byte: vertex indices
-    6
-
-EDGE:
-    2 byte: vertexids
-    2 byte: faceids
-    2 byte: edgeids left and right of vertex[0]
-    2 byte: edgeids left and right of vertex[1]
+UV:
+    4 byte: position (2 * signed short) (normalized from 0 to 65535)
+    2 byte: vertid
+    2 byte: padding
     8
 
-MSH:
-    HEADER,
-    VERTS,
-    FACES,
-    EDGES
-"""
+FACE:
+    6 byte: uv indices
+    2 byte: edgeid
+    8
 
-slicesUvs = True
+EDGE:
+    4 byte: vertid * 2
+    4 byte: faceid * 2 (left, then right of vert[0])
+    2 byte: cw edge from vert[0]
+    2 byte: ccw edge from vert[0]
+    2 byte: cw edge from vert[1]
+    2 byte: ccw edge from vert[1]
+    16
+
+MSH:
+    HEADER
+    'VERT'
+    VERTS
+    'UVUV'
+    UV
+    'FACE'
+    FACES
+    'EDGE'
+    EDGES
+    'BONE'
+    BONES
+"""
 
 #
 # SHARELIB
@@ -87,65 +103,6 @@ def float_to_ubyte(val):
         return 0
     return int(round(val * (2**8-1)))
 
-#
-#
-#
-
-def bone_weight_normalize(bones):
-    BONEW1 = 2; BONEW2 = 3
-    b_sum = bones[BONEW1] + bones[BONEW2]
-    if b_sum > 0:
-        bones[BONEW1] = float_to_ubyte(bones[BONEW1] / b_sum)
-        bones[BONEW2] = float_to_ubyte(bones[BONEW2] / b_sum)
-    else:
-        bones[BONEW1] = 0
-        bones[BONEW2] = 0
-    return bones
-
-def bone_id_of_group(obj, groupid, blist):
-    BONE = 3
-    nm = obj.vertex_groups[groupid].name
-    for i in range(0, len(blist)):
-        if(nm == blist[i][BONE].name):
-            print(nm + " is group " + str(i));
-            return i
-    return None
-
-def vert_get_bones(obj, vert, blist):
-    boneid = [255, 255]
-    bonew = [0.0, 0.0]
-    for group in vert.groups:
-        g_boneid = bone_id_of_group(obj, group.group, blist)
-        if g_boneid != None:
-            if group.weight > bonew[0]:
-                bonew[1] = bonew[0]
-                boneid[1] = boneid[0]
-                bonew[0] = group.weight
-                boneid[0] = g_boneid
-            elif group.weight > bonew[1]:
-                bonew[1] = group.weight
-                boneid[1] = g_boneid
-    return bone_weight_normalize([boneid[0], boneid[1], bonew[0], bonew[1]])
-
-def find_bone_parentid(arm, bone):
-    if(bone.parent):
-        for i in range(len(arm.data.bones)):
-            if(arm.data.bones[i] == bone.parent):
-                return i
-    return 255
-
-def get_bone_list(obj):
-    armature = obj.find_armature()
-    blist = []
-    if(armature):
-        for i in range(0, len(armature.data.bones)):
-            bone = armature.data.bones[i]
-            pid = find_bone_parentid(armature, bone)
-            blist.append([bone.name, i, pid, bone])
-    return blist
-
-####
-
 class Vert(object):
     def __init__(self, bmv):
         self.bmv = bmv
@@ -153,37 +110,47 @@ class Vert(object):
     def __getattr__(self, name):
         return getattr(self.bmv, name)
 
+    def serialize(self):
+        rows = [[1, 0, 0, 0],
+                [0, 0, 1, 0],
+                [0,-1, 0, 0],
+                [0, 0, 0, 1]]
+        tmat = Matrix(rows)
+        co = tmat * self.bmv.co
+        normal = tmat * self.bmv.normal
+        vpack = struct.pack("fffhhhHHBBBBBBBBH",
+                        co.x,
+                        co.y,
+                        co.z,
+                        float_to_short(normal.x),
+                        float_to_short(normal.y),
+                        float_to_short(normal.z),
+                        0, 0, # padding
+                        0, 0, 0, # vert color
+                        0, # material id
+                        0, 0, # bone ids
+                        0, 0, # bone weights
+                        self.bmv.link_edges[0].index)
+        assert(len(vpack) == 32)
+        return vpack
+
 class Face(object):
     def __init__(self, bmf):
         self.bmf = bmf
+        self.uvs = []
 
     def __getattr__(self, name):
         return getattr(self.bmf, name)
 
-    def getUv(self, layer, i):
-        luv = self.bmf.loops[i][layer].uv
-        return Uv(luv.x, luv.y, self.bmf.loops[i].vert.index, None) # kinda weird, i'm not adding color to this UV
+    def serialize(self):
+        buf = []
 
-# vert_index, 0 or 1. first or second vert in edge
-def get_edge_prevwing(bmedge, vert_index):
-    loop = bmedge.link_loops[1] # its either one or the other
-    if bmedge.verts[vert_index].index == bmedge.link_loops[0].vert.index:
-        loop = bmedge.link_loops[0]
-    return loop.link_loop_prev.edge
-
-def get_edge_nextwing(bmedge, vert_index):
-    loop = bmedge.link_loops[1] # its either one or the other
-    if bmedge.verts[vert_index].index == bmedge.link_loops[0].vert.index:
-        loop = bmedge.link_loops[0]
-    return loop.link_loop_next.edge
-
-def get_edge_vertid(bmedge, vert_index):
-    return bmedge.verts[vert_index].index
-
-def get_edge_faceid(bmedge, face_index):
-    # TODO: check if this is ordered consistantly
-    # I suspect bmesh makes no guarentee that faces[0] is left-wise (as i would expect)
-    return bmedge.link_faces[face_index].index 
+        for i in range(0, 3):
+            buf.append(struct.pack("H", self.uvs[i]))
+        buf.append(struct.pack("H", self.bmf.edges[0].index))
+        pack = b''.join(buf)
+        assert(len(pack) == 8)
+        return pack
 
 class Edge(object):
     def __init__(self, bme):
@@ -191,6 +158,40 @@ class Edge(object):
 
     def __getattr__(self, name):
         return getattr(self.bme, name)
+
+    # vert_index, 0 or 1. first or second vert in edge
+    def get_prevwingid(self, vert_index):
+        loop = self.bme.link_loops[1] # its either one or the other
+        if self.bme.verts[vert_index].index == self.bme.link_loops[0].vert.index:
+            loop = self.bme.link_loops[0]
+        return loop.link_loop_prev.edge.index
+
+    def get_nextwingid(self, vert_index):
+        loop = self.bme.link_loops[1] # its either one or the other
+        if self.bme.verts[vert_index].index == self.bme.link_loops[0].vert.index:
+            loop = self.bme.link_loops[0]
+        return loop.link_loop_next.edge.index
+
+    def get_vertid(self, vert_index):
+        return self.bme.verts[vert_index].index
+
+    def get_faceid(self, face_index):
+        # TODO: check if this is ordered consistantly
+        # I suspect bmesh makes no guarentee that faces[0] is left-wise (as i would expect)
+        return self.bme.link_faces[face_index].index
+
+    def serialize(self):
+        epack = struct.pack("HHHHHHHH",
+                            self.get_vertid(0),
+                            self.get_vertid(1),
+                            self.get_faceid(0),
+                            self.get_faceid(1),
+                            self.get_prevwingid(0),
+                            self.get_nextwingid(0),
+                            self.get_prevwingid(1),
+                            self.get_nextwingid(1))
+        assert(len(epack) == 16)
+        return epack
 
 class Uv(object):
     def __init__(self, uvx, uvy, vindex, color, material=0):
@@ -209,6 +210,13 @@ class Uv(object):
     def __repr__(self):
         return "(Uv " + str(self.uvx) + ", " + str(self.uvy) + ", " + str(self.vindex) + ", " + str(self.color) + ")"
 
+    def serialize(self):
+        pack = struct.pack("HHHH",
+                self.uvx, self.uvy, # uvs
+                self.vindex, 0)
+        assert(len(pack) == 8)
+        return pack
+
 
 class Mesh(object):
     def __init__(self, mesh, settings):
@@ -223,144 +231,78 @@ class Mesh(object):
         self.verts = []
         self.faces = []
         self.edges = []
+        self.bones = []
         self.uvs = dict()
 
         self.uv_layer = self.bm.loops.layers.uv.verify()
         self.color_layer = self.bm.loops.layers.color.verify()
 
-        for v in self.bm.verts:
-            self.verts.append(Vert(v))
+        for bmv in self.bm.verts:
+            self.verts.append(Vert(bmv))
 
-        for f in self.bm.faces:
-            self.faces.append(Face(f))
+        for bmf in self.bm.faces:
+            f = Face(bmf)
 
             # make set of uvs for each face
             for l in f.loops:
-                luvx = 0
-                luvy = 0
-                if settings['sliceUvs']: # if we dont add this, uvs are unique on vertid
-                    luvx = l[self.uv_layer].uv.x
-                    luvy = l[self.uv_layer].uv.y
+                uvx = l[self.uv_layer].uv.x
+                uvy = l[self.uv_layer].uv.y
                 color = l[self.color_layer]
-                iuv = Uv(luvx, luvy, l.vert.index, color, f.material_index)
+                iuv = Uv(uvx, uvy, l.vert.index, color, f.material_index)
                 if iuv not in self.uvs:
-                    self.uvs[iuv] = len(self.uvs) # add uv to set, value is uv index
+                    iuv.index = len(self.uvs)
+                    self.uvs[iuv] = iuv
+                f.uvs.append(self.uvs[iuv].index) # append UV id to face
 
-        for e in self.bm.edges:
-            self.edges.append(Edge(e))
+            self.faces.append(f)
+
+        for bme in self.bm.edges:
+            self.edges.append(Edge(bme))
 
     def __getattr__(self, name):
         return getattr(self.bm, name)
 
-    def uv_layer(self):
-        return self.bm.loops.layers.uv.verify()
-
-    def nverts(self):
-        return len(self.uvs)
-
-    def nfaces(self):
-        return len(self.faces)
-
-    def nedges(self):
-        if not self.settings['sliceUvs']:
-            return len(self.edges)
-        return 0
-
     def serialize(self):
         buf = []
         buf.append(self.serialize_header())
-        buf.append(self.serialize_verts())
-        buf.append(self.serialize_faces())
-        if not self.settings['sliceUvs']:
-            buf.append(self.serialize_edges())
+
+        buf.append(self.serialize_label("VERT"))
+        for v in self.verts:
+            buf.append(v.serialize())
+
+        buf.append(self.serialize_label("UVUV"))
+        for uv in self.uvs.values():
+            buf.append(uv.serialize())
+
+        buf.append(self.serialize_label("FACE"))
+        for f in self.faces:
+            buf.append(f.serialize())
+
+        buf.append(self.serialize_label("EDGE"))
+        for e in self.edges:
+            buf.append(e.serialize())
+
+        buf.append(self.serialize_label("BONE"))
+        # TODO bones
+
         return b''.join(buf)
+
+    def serialize_label(self, label):
+        fmt = "4s"
+        pack = struct.pack(fmt, bytes(label, 'utf-8'))
+        return pack
 
     def serialize_header(self):
-        hfmt = "3sBIIIB15s"
-        hpack = struct.pack(hfmt, b"MDL", 5,
-                    self.nverts(),
-                    self.nfaces(),
-                    self.nedges(),
-                    0,               # number of bones
-                    bytes(self.mesh.name, "UTF-8"))
+        hfmt = "3sBHHHHHxx15sB"
+        hpack = struct.pack(hfmt, b"MDL", 6,
+                    len(self.verts),
+                    len(self.uvs),
+                    len(self.faces),
+                    len(self.edges),
+                    len(self.bones),
+                    bytes(self.mesh.name, "UTF-8"), 0)
         assert(len(hpack) == 32)
         return hpack
-
-    def serialize_uv(self, uv):
-        v = self.bm.verts[uv.vindex]
-        rows = [[1, 0, 0, 0],
-                [0, 0, 1, 0],
-                [0,-1, 0, 0],
-                [0, 0, 0, 1]]
-        tmat = Matrix(rows)
-        co = tmat * v.co
-        normal = tmat * v.normal
-        vpack = struct.pack("fffhhhHHBBBBBBBBH",
-                        co.x,
-                        co.y,
-                        co.z,
-                        float_to_short(normal.x),
-                        float_to_short(normal.y),
-                        float_to_short(normal.z),
-                        uv.uvx, uv.uvy, # uvs
-                        float_to_ubyte(uv.color.r), 
-                        float_to_ubyte(uv.color.g),
-                        float_to_ubyte(uv.color.b), 
-                        0, # material id
-                        0, 0, # bone ids
-                        0, 0, # bone weights
-                        v.link_edges[0].index)
-        assert(len(vpack) == 32)
-        return vpack
-
-    def serialize_verts(self):
-        buf = []
-        for uv in sorted(self.uvs, key=self.uvs.get): # should be incremental
-            buf.append(self.serialize_uv(uv))
-        return b''.join(buf)
-
-    def serialize_face(self, f):
-        buf = []
-
-        # if there are no UVs, the a UV just represents a vert
-        for i in range(0, 3):
-            uv = f.getUv(self.uv_layer, i)
-            # if we shouldn't slice uvs, dont use uv's provided by face
-            if not self.settings['sliceUvs']:
-                uv.uvx = 0 
-                uv.uvy = 0
-            buf.append(struct.pack("H", self.uvs[uv]))
-        # buf.append(struct.pack("H", f.edges[0].index)) # cannot add edge reference because it breaks OpenGL
-        fpack = b''.join(buf)
-        assert(len(fpack) == 6)
-        return fpack
-
-    def serialize_faces(self):
-        buf = []
-        for f in self.faces:
-            buf.append(self.serialize_face(f))
-        return b''.join(buf)
-
-    # only valid if not using UVs
-    def serialize_edge(self, e):
-        epack = struct.pack("HHHHHHHH",
-                            get_edge_vertid(e, 0),
-                            get_edge_vertid(e, 1),
-                            get_edge_faceid(e, 0),
-                            get_edge_faceid(e, 1),
-                            get_edge_prevwing(e, 0).index,
-                            get_edge_nextwing(e, 0).index,
-                            get_edge_prevwing(e, 1).index,
-                            get_edge_nextwing(e, 1).index)
-        assert(len(epack) == 16)
-        return epack
-
-    def serialize_edges(self):
-        buf = []
-        for e in self.edges:
-            buf.append(self.serialize_edge(e))
-        return b''.join(buf)
-            
 
 import logging
 def serialize_mesh(obj, settings):
@@ -389,30 +331,6 @@ class MdlExport(Operator, ExportHelper):
             default="*.msh",
             options={'HIDDEN'})
 
-    sliceUvs = BoolProperty(
-            name="Slice UV mapping",
-            description="If true, vertices will be split so there "
-                        "is one vertex entry per unique UV", 
-            default=True,)
-
-    # List of operator properties, the attributes will be assigned
-    # to the class instance from the operator settings before calling.
-    """
-    use_setting = BoolProperty(
-           name="Example Boolean",
-            description="Example Tooltip",
-            default=True,
-            )
-
-    type = EnumProperty(
-            name="Example Enum",
-            description="Choose between two items",
-            items=(('OPT_A', "First Option", "Description one"),
-                   ('OPT_B', "Second Option", "Description two")),
-            default='OPT_A',
-            )
-    """
-
     def execute(self, context):
         if not context.object.type == "MESH":
             raise Exception("Mesh must be selected, " + context.object.type + " was given")
@@ -420,7 +338,7 @@ class MdlExport(Operator, ExportHelper):
         obj = context.object
 
         f = open(self.filepath, 'wb')
-        obuf = serialize_mesh(obj, {'sliceUvs': self.sliceUvs})
+        obuf = serialize_mesh(obj, {})
         f.write(obuf)
         f.close()
         return {'FINISHED'}
